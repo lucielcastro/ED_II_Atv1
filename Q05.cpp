a13ccf9965e88257860f7d7fe4bf33f053c45e5c
lucielcastro <93848647+lucielcastro@users.noreply.github.com>
2022-04-24 19:43:11 -0300

    Add files via upload

--- Q05.cpp
+++ Q05.cpp
@@ -1,55 +1,31 @@
-#include<stdio.h>
-#include<conio.h>
-#include<math.h>
-#include<string>
+#include <stdio.h>
+#include <math.h>
+#include <conio.h>
 
-typedef struct {
-      float x;
-      float y;
-      float z;
-} Vet_R3;
-
-void ImprimePessoa(Vet_R3 P)
-{   
-    printf("\nX: %.2f\nY: %.2f\nZ: %.2f\n",P.x, P.y, P.z);
-}
-void setPessoa(Vet_R3 *p, float X, float Y, float Z)
-{
-      p->x = X;
-      p->y = Y;
-      p->z = Z;
-}
 int main()
 {
-     Vet_R3 vetor[2];
-     int i;
-     float somatorio_X_vet = 0, somatorio_Y_vet = 0, somatorio_Z_vet = 0;
-     for(i=0; i<2; i++){
-    
-     printf("\nInforme X do vetor %d: ", i+1);
-     scanf("%f", &vetor[i].x);
+    float nota1, nota2, media;
 
-     printf("Informe Y do vetor %d: ", i+1);
-     scanf("%f", &vetor[i].y);
+    printf("\nInforme nota 1: ");
+    scanf("%f", &nota1);
 
-     printf("Informe Z do vetor %d: ", i+1);
-     scanf("%f", &vetor[i].z);
-     
-      somatorio_X_vet = somatorio_X_vet + vetor[i].x;
-      somatorio_Y_vet = somatorio_Y_vet + vetor[i].y;
-      somatorio_Z_vet = somatorio_Z_vet + vetor[i].z;
-     }
-     
-     for(i=0; i<2; i++)
-     setPessoa(&vetor[i], vetor[i].x, vetor[i].y, vetor[i].z);
-     
-     for(i=0; i<2; i++)
-     ImprimePessoa(vetor[i]);
-     
-     printf("\nNovo vetor: X: %.2f Y: %.2f Z %.2f", somatorio_X_vet, somatorio_Y_vet, somatorio_Z_vet);
-     printf("\n");
+    while (nota1 != 50) {
     
-     
+    printf("\nInforme nota 2: ");
+    scanf("%f", &nota2);
+
+        while ((nota1 < 0 || nota1 > 10) || (nota2 < 0 || nota2 > 10)) {
+            printf("\nAlguma nota invalida, iforme as notas novamente: ");
+            scanf("%f", &nota1);
+            while (nota1 == 50) break;
+            scanf("%f", &nota2);
+        }
+        media = (nota1 + nota2) / 2;
+        printf("\nMedia = %.2f", media);
+        
+        printf("\nInforme as duas notas do outro aluno: ");
+        scanf("%f%f", &nota1, &nota2);
+    }
     printf("\nPressione qualquer tecla para finalizar!");
     getch();
 }
