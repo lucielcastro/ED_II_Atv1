9cc93c203df173fcf1db4d5206e9a11219f87262
lucielcastro <93848647+lucielcastro@users.noreply.github.com>
2022-05-07 14:17:59 -0300

    Add files via upload

--- Q03.cpp
+++ Q03.cpp
@@ -2,27 +2,34 @@
 #include<math.h>
 #include<conio.h>
 
+/*Faça um programa em "C" que lê dois valores e imprime: 
+- se o primeiro valor for menor que o segundo, a lista de valores do primeiro até o segundo; 
+- se o primeiro valor for maior que o segundo a lista de valores do primeiro até o segundo em ordem decrescente; 
+- se ambos forem iguais a mensagem "valores iguais".*/
+
 int main(){
 
-    int n1, n2;
+     int n1, n2;
 
-    printf("\nInforme dois numeros inteiros: ");
-    scanf("%d %d", &n1, &n2);
+     printf("\nInforme dois numeros inteiros: ");
+     scanf("%d %d", &n1, &n2);
     
-    printf("\n");
+     printf("\n");
     
-    if(n1<n2) 
-    {
-       for(int i = n1; i<=n2; i++)
-       printf("%d ", i);
-    }
-    else if(n1>n2) 
-    {
-        for(int i = n1; i>=n2; i--)
-        printf("%d ", i);
-    }
-    else printf("\n\tNumeros iguais\n");
+     if(n1<n2) 
+     {
+         for(int i = n1; i<=n2; i++)
+         printf("%d ", i);
+     }
+     else if(n1>n2) 
+     {
+         for(int i = n1; i>=n2; i--)
+         printf("%d ", i);
+     }
+     else printf("\n\tNumeros iguais\n");
 
-    printf("\n\nPressione qualquer tecla para finalizar!");
-    getch();
+     printf("\n\nPressione qualquer tecla para finalizar!");
+     getch();
+
+     return 0;
 }
